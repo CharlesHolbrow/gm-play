21a23ef1008ae946cb80a382599ea94ac908a88e
Charles Holbrow <charlesholbrow@gmail.com>
2018-02-05 16:17:46 -0500

    Refactor: moving `Pattern` to `m` package

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"time"
 
@@ -9,6 +8,17 @@ import (
 	"github.com/CharlesHolbrow/m"
 	"github.com/rakyll/portmidi"
 )
+
+// AddNotePattern adds notes (with the given pattern) to a sequence
+func (s *Sequence) AddNotePattern(notes m.NoteGroup, pattern *m.Pattern, midiCh uint8) {
+	for i, note := range notes {
+		pEvent := pattern.Get(i)
+		start := pEvent.StartPosition
+		end := start + pEvent.Duration
+		s.Add(start, gm.Note{On: true, Note: note, Vel: uint8(pEvent.Value), Ch: midiCh})
+		s.Add(end, gm.Note{Note: note, Ch: midiCh})
+	}
+}
 
 func main() {
 	if err := portmidi.Initialize(); err != nil {
@@ -20,27 +30,53 @@ func main() {
 		log.Fatal(err)
 	}
 
-	cMinor := m.Append(m.MinorTriad(m.C).AllOctaves().Over(m.C2).AllSubgroups(7)[:12]...)
-	cMinor = cMinor.Append(cMinor.Reverse())
-	fMinor := cMinor.Transpose(5)
-	bFlatMinor := fMinor.Transpose(-7)
-	// afMajor := m.Append(m.MajorTriad(m.Ab).AllOctaves().Over(m.C3).AllSubgroups(7)[:12]...)
-	// final := afMajor.Interleave(cMinor)
-	final := cMinor.Append(fMinor, bFlatMinor).Repeat(1)
-	fmt.Println(len(final))
+	// Melodies
+	cMinorSubgroups := m.MinorTriad(m.G).AllOctaves().Over(m.G4).AllSubgroups(8)
+	cMinor := m.Append(cMinorSubgroups[:4]...)
+	final := cMinor.Append(cMinor.Transpose(6), cMinor.Transpose(7), cMinor.Transpose(-5))
 
+	roots := m.Group(m.G3, m.E4, m.C4, m.C4) // Bassline
+	roots = roots.Interleave(roots)
+
+	// Patterns
+	pBass := m.NewPattern(2)
+	pBass.Push(1.5, 100).Advance(1.5).Push(0.5, 100)
+
+	pSevenths := m.NewPatternSubdivisions(16, 2)
+	pSevenths.RampValue(127, 2)
+
+	// Create Sequence
 	s := NewSequence()
 
-	for i, number := range final {
-		s.Add(float64(i*30), gm.Note{On: true, Note: number, Vel: 120})
-		s.Add(float64(i*30+20), gm.Note{Note: number})
-	}
+	s.AddNotePattern(roots, pBass, 1)
+	s.AddNotePattern(final, pSevenths, 0)
 
-	for event := range s.Play(time.Millisecond) {
+	for event := range s.Play(time.Second) {
 		switch e := event.(type) {
 		case gm.Note:
 			out.WriteShort(e.Midi())
 		}
 	}
+
+	// cMinor = cMinor.Append(cMinor.Reverse())
+	// fMinor := cMinor.Transpose(5)
+	// bFlatMinor := fMinor.Transpose(-7)
+
+	// final := cMinor.Append(fMinor, bFlatMinor).Repeat(1)
+	// fmt.Println(len(final))
+
+	// s := NewSequence()
+
+	// for i, number := range final {
+	// 	s.Add(float64(i*20), gm.Note{On: true, Note: number, Vel: 120})
+	// 	s.Add(float64(i*20+20), gm.Note{Note: number})
+	// }
+
+	// for event := range s.Play(time.Millisecond) {
+	// 	switch e := event.(type) {
+	// 	case gm.Note:
+	// 		out.WriteShort(e.Midi())
+	// 	}
+	// }
 	time.Sleep(10 * time.Millisecond)
 }
