1a71ca5e91f878cc5240c5811e0d7ca5946e4ef2
Charles Holbrow <charlesholbrow@gmail.com>
2018-02-06 18:47:46 -0500

    organizing

--- main.go
+++ main.go
@@ -24,6 +24,7 @@ func main() {
 	}
 
 	// Reaper doesn't obey cc123. Send a note off message on every note.
+	// BUG(charles) output stream should be locked with a mutex
 	go func() {
 		c := make(chan os.Signal, 1)
 		signal.Notify(c, syscall.SIGINT)
@@ -41,31 +42,38 @@ func main() {
 		os.Exit(0)
 	}()
 
-	// Melodies
-	// cMinorSubgroups := m.MinorTriad(m.G).AllOctaves().Over(m.G4).AllSubgroups(8)
-	// cMinor := m.Append(cMinorSubgroups[:4]...)
-	// final := cMinor.Append(cMinor.Transpose(6), cMinor.Transpose(7), cMinor.Transpose(-5))
+	// Chip Melody
+	cMinorSubgroups := m.MinorTriad(m.G).AllOctaves().Over(m.G4).AllSubgroups(7)
+	cMinor := m.Append(cMinorSubgroups[:4]...)
+	chipMelody := cMinor.Append(cMinor.Transpose(6), cMinor.Transpose(7), cMinor.Transpose(-5))
 
-	// Melody
-	notes := m.Group(m.G3, m.E4, m.C4, m.C4).Repeat(40) // Bassline
-	notes = notes.Interleave(notes)
+	// Chip Pattern
+	chipPattern := m.NewSequence()
+	chipPattern.AddSubdivisions(14, 1, 1.)
+	chipPattern.Cursor = 1
+	chipPattern.RampSustainVelocity(100, 0)
 
-	// Patterns
-	rhythm := m.NewSequence()
-	rhythm.AddSustain(0, 1.5, 100)
-	rhythm.AddSustain(1.5, 0.5, 50)
-	rhythm.Cursor = 2
+	// Bass Melody
+	bassMelody := m.Group(m.G3, m.E4, m.C4, m.C4) // Bassline
+	bassMelody = bassMelody.Interleave(bassMelody)
+
+	// Bass Pattern
+	bassPattern := m.NewSequence()
+	bassPattern.AddSustain(0, 1.5, 100)
+	bassPattern.AddSustain(1.5, 0.5, 50)
+	bassPattern.Cursor = 2
 
 	s := m.NewSequence()
-	s.AddRhythmicMelody(rhythm, notes, 1)
+	s.AddRhythmicMelody(chipPattern, chipMelody, 0)
+	s.AddRhythmicMelody(bassPattern, bassMelody, 1)
 
 	// pSevenths := m.NewPatternSubdivisions(16, 2)
 	// pSevenths.RampValue(127, 2)
 
-	for event := range s.Play(time.Second) {
+	for event := range s.Play(time.Millisecond * 1050) {
 		switch e := event.(type) {
 		case gm.Note:
-			fmt.Println(e.Midi())
+			// fmt.Println(e.Midi())
 			out.WriteShort(e.Midi())
 		}
 	}
