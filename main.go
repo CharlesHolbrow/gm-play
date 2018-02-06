f50fab0719927cf0a25f20d42273e0e8de36c691
Charles Holbrow <charlesholbrow@gmail.com>
2018-02-06 13:13:54 -0500

    moving `Sequence` to `m` package

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/CharlesHolbrow/gm"
@@ -20,41 +23,52 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Reaper doesn't obey cc123. Send a note off message on every note.
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT)
+
+		// Block until a signal is received.
+		<-c
+		fmt.Println("\nSending note-off event on each midi note")
+		for ch := uint8(0); ch <= 15; ch++ {
+			for n := uint8(0); n <= 127; n++ {
+				out.WriteShort(gm.Note{Note: n, Ch: ch}.Midi())
+			}
+		}
+		time.Sleep(10 * time.Millisecond)
+		portmidi.Terminate()
+		os.Exit(0)
+	}()
+
 	// Melodies
 	// cMinorSubgroups := m.MinorTriad(m.G).AllOctaves().Over(m.G4).AllSubgroups(8)
 	// cMinor := m.Append(cMinorSubgroups[:4]...)
 	// final := cMinor.Append(cMinor.Transpose(6), cMinor.Transpose(7), cMinor.Transpose(-5))
 
 	// Melody
-	roots := m.Group(m.G3, m.E4, m.C4, m.C4).Repeat(40) // Bassline
-	roots = roots.Interleave(roots)
+	notes := m.Group(m.G3, m.E4, m.C4, m.C4).Repeat(40) // Bassline
+	notes = notes.Interleave(notes)
 
 	// Patterns
-	pBass := NewSequence(2)
-	pBass.Add(0, m.Note{Vel: 100, Length: 1.5})
-	pBass.Add(1.5, m.Note{Vel: 100, Length: 0.5})
+	rhythm := m.NewSequence()
+	rhythm.AddSustain(0, 1.5, 100)
+	rhythm.AddSustain(1.5, 0.5, 50)
+	rhythm.Cursor = 2
 
-	melody := NewSequence(1)
-	midiCh := uint8(1)
-	for i, root := range roots {
-		seqEvent := pBass.Get(i)
-		if m, ok := seqEvent.Event.(m.Note); ok {
-			melody.Add(seqEvent.position, gm.Note{On: true, Note: root, Ch: midiCh, Vel: 100})
-			melody.Add(seqEvent.position+m.Length, gm.Note{Note: root, Ch: midiCh})
-		}
-	}
+	s := m.NewSequence()
+	s.AddRhythmicMelody(rhythm, notes, 1)
 
 	// pSevenths := m.NewPatternSubdivisions(16, 2)
 	// pSevenths.RampValue(127, 2)
 
-	// Create Sequence
-
-	for event := range melody.Play(time.Second) {
+	for event := range s.Play(time.Second) {
 		switch e := event.(type) {
 		case gm.Note:
 			fmt.Println(e.Midi())
 			out.WriteShort(e.Midi())
 		}
 	}
+
 	time.Sleep(10 * time.Millisecond)
 }
